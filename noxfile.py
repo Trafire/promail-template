ccc6cc513a2fc529818d5974cf500fdd60c28e8f
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-05-28 22:30:17 -0400

    coverage session added

--- noxfile.py
+++ noxfile.py
@@ -111,6 +111,14 @@ def docs(session: Session) -> None:
     install_with_constraints(session, "sphinx", "sphinx-autodoc-typehints")
     session.run("sphinx-build", "docs", "docs/_build")
 
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
+
+
 
 @nox.session(python=["3.8"])
 def poetry_update(session: Session) -> None:
